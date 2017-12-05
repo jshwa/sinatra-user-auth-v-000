1c8ff0febf1b64193414ef1517b21ec12bd631b5
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-05 00:19:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find(params)
+    @user = User.findby(params)
     session[:id] = @user.id
     redirect '/users/home'
   end
